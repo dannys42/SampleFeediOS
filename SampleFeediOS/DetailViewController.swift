5142c48953ee176980898edadc39118995546229
Danny Sung <danny@dannysung.com>
2020-02-01 17:00:03 -0800

    Add Master-Detail template iOS app

---
+++ SampleFeediOS/DetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DetailViewController.swift
+//  SampleFeediOS
+//
+//  Created by Danny Sung on 02/01/2020.
+//  Copyright © 2020 Sung Heroes. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.timestamp!.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        configureView()
+    }
+
+    var detailItem: Event? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
